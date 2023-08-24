930d034fb302db6d45b83f3f46302791a9b636c9
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2023-08-24 12:44:35 +0300

    fix: fix deploying error

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -7,6 +8,9 @@ const parcelsRouter = require('./routes/api/parcels');
 
 const app = express();
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
